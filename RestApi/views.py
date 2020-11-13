79b49dd9b8085d75d6c92d9d7d8daa60ac7650af
aayushrai <www.aayushrai@gmail.com>
2020-11-13 15:47:17 +0530

    Api compelete

--- RestApi/views.py
+++ RestApi/views.py
@@ -7,28 +7,29 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import imaplib, email, os
+import re
 
 #https://www.google.com/settings/security/lesssecureapps
 @api_view(["POST"])
 def sendEmail(request):
-    sender_email = "www.aayushrai@gmail.com"
-    password = "9893226631"
+    sender_email = "dummy21072000@gmail.com"
+    password = "Aayush#21"
+    data = request.data
     message = MIMEMultipart("alternative")
-    message["Subject"] = "Your Coffe Shop Order: Placed"
+    message["Subject"] = data["Subject"]
     message["From"] = sender_email
-    part1 = MIMEText("Text which you wnat to send", "plain")
+    part1 = MIMEText(data["Content"], "plain")
     # part2 = MIMEText(html, "html")
     message.attach(part1)
     # message.attach(part2)
-    receivers = ["rai.aayush2000@gmail.com"]
+    receiver_email = data["To"]
     context = ssl.create_default_context()
-    if len(receivers)>0:
+    if receiver_email:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
                 print("Sending emails")
                 server.login(sender_email, password)
-                for receiver_email in receivers:
-                        message["To"] = receiver_email
-                        server.sendmail(sender_email, receiver_email, message.as_string())
+                message["To"] = receiver_email
+                server.sendmail(sender_email, receiver_email, message.as_string())
     else:
         print("No Emails")
     return Response({"email":"ohk"})
@@ -37,24 +38,25 @@ def sendEmail(request):
 
 @api_view(["GET"])
 def getMessages(request):
-    user = "www.aayushrai@gmail.com"
-    password = "9893226631"
+    user = "dummy21072000@gmail.com"
+    password = "Aayush#21"
     imap_url = "imap.gmail.com"
     connection = imaplib.IMAP4_SSL(host=imap_url)
     connection.login(user, password)
     connection.select()
+   
+
     result, data = connection.uid('search', None, "ALL")
+    lst = []
     if result == 'OK':
         for num in reversed(data[0].split()):
             result, data = connection.uid('fetch', num, '(RFC822)')
             if result == 'OK':
                 email_message = email.message_from_bytes(data[0][1])
-                print('From:' + email_message['From'])
-                print('To:' + email_message['To'])
-                print('Date:' + email_message['Date'])
-                print('Subject:' + str(email_message['Subject']))
-                print('Content:' + str(email_message.get_payload()[0]))
+                filter_content = re.sub("^[\s\S]*?[\n\r][\n\r]", "", str(email_message.get_payload()[0]))
+                dis = {'From' : email_message['From'],'To' : email_message['To'],'Date' : email_message['Date'],'Subject' : str(email_message['Subject']),'Content' : filter_content}
+                lst.append(dis)
             
     connection.close()
     connection.logout()
-    return Response({"email":"this my email"})
+    return Response(lst)
